1db1bcac699053cfda090c365a2af26867187e9d
Arman Bhimani <Armanbhimani@gmail.com>
2019-07-12 14:50:31 -0500

    Removed Button

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -16,7 +16,6 @@ function Posts(props) {
                             <p>{props.user.start_date}</p>
                         </header>
                         <p>{post.content}</p>
-                        <button>Comment</button>
                     </section>
                 )
             })}
@@ -27,7 +26,6 @@ function Posts(props) {
                     <p>07.08.2019</p>
                 </header>
                 <p>Praesent sagittis a mi sit amet dictum. Donec orci nibh, dignissim in leo et, congue semper mauris. Donec elit lacus, dictum et placerat eget, rhoncus sodales erat. Curabitur sit amet placerat neque, a tempus mi. Suspendisse a tempus dolor. Nullam porttitor nisi sed justo dictum consequat. Etiam sed congue felis.</p>
-                <button>Comment</button>
             </section>
             <section>
                 <header>
@@ -36,7 +34,6 @@ function Posts(props) {
                     <p>01.27.2017</p>
                 </header>
                 <p>Praesent sagittis a mi sit amet dictum. Donec orci nibh, dignissim in leo et, congue semper mauris. Donec elit lacus, dictum et placerat eget, rhoncus sodales erat. Curabitur sit amet placerat neque, a tempus mi. Suspendisse a tempus dolor. Nullam porttitor nisi sed justo dictum consequat. Etiam sed congue felis.</p>
-                <button>Comment</button>
             </section>
             <section>
                 <header>
@@ -45,7 +42,6 @@ function Posts(props) {
                     <p>01.26.2017</p>
                 </header>
                 <p>Praesent sagittis a mi sit amet dictum. Donec orci nibh, dignissim in leo et, congue semper mauris. Donec elit lacus, dictum et placerat eget, rhoncus sodales erat. Curabitur sit amet placerat neque, a tempus mi. Suspendisse a tempus dolor. Nullam porttitor nisi sed justo dictum consequat. Etiam sed congue felis.</p>
-                <button>Comment</button>
             </section>
         </main>
     )
